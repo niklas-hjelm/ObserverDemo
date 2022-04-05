d298dff7f96eb68b4e4ba3ffd6fa9b8ce7fce149
niklas-hjelm <niklas.hjelm@iths.se>
2022-04-05 13:57:09 +0200

    lkjljlasjldjlasjdlkjaslkjdlkasj

---
+++ ObserverDemo/ObserverPattern/PremiumSub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObserverDemo.ObserverPattern.Interfaces;
+
+namespace ObserverDemo.ObserverPattern
+{
+    public class PremiumSub : IObserver
+    {
+        public void Update(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ObserverDemo/Program.cs
+++ ObserverDemo/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using ObserverDemo.ObserverPattern;
+using ObserverDemo.ObserverPattern.Interfaces;
+
+ISubject blog = new Blog();
+
+IObserver niklas = new Subscriber("Niklas");
+IObserver kalle = new Subscriber("Kalle");
+IObserver pelle = new Subscriber("Pelle");
+
+blog.Subscribe(niklas);
+blog.Subscribe(kalle);
+blog.Subscribe(pelle);
+
+blog.Upload("Byhåla");
+
+blog.UnSubscribe(kalle);
+
+blog.Upload("Byhåla 2");
